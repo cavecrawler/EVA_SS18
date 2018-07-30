83e3688c26784490061f61991c0b95b69575ed12
cydietz <cy.dietz@googlemail.com>
2018-07-30 13:45:47 +0200

    - added ConfigReader for config_wp.txt

---
+++ src/main/java/Main/ConfigReader.java
@@ -0,0 +1,31 @@
+package Main;
+
+import sun.security.krb5.Config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    protected static Properties prop;
+    private InputStream inputStream;
+
+    public ConfigReader() {
+
+        prop = new Properties();
+        inputStream = ConfigReader.class.getClassLoader().getResourceAsStream("config_wp.txt");
+
+        try {
+            prop.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen von config_wp.txt");
+        }
+    }
+
+    public int getSelectedIndicator() {
+        Integer i = Integer.valueOf(prop.getProperty("YoY"));
+        return i;
+    }
+}
