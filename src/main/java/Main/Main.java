d8d098f635e80b84c9bc4372a125bc76ebf5f24b
cydietz <cy.dietz@googlemail.com>
2018-07-31 11:08:29 +0200

    - added relative resource path via config

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -13,28 +13,20 @@ public class Main {
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
         ConfigReader config = new ConfigReader();
-        String XLS_Filepath = "C:\\Users\\chrisso\\Documents\\JavaProjects\\src\\main\\resources\\ROhdaten.xlsx";
-        ExcelLoader excelLoader = new ExcelLoader(XLS_Filepath);
+        ExcelLoader excelLoader = new ExcelLoader(config.getFilePath());
 
         Workbook workbook = excelLoader.getWorkbook(); //workbook holen
-        WorkbookReader workbookReader = new WorkbookReader(workbook, 0); //workbookReader starten
-        NumberSetList numberSetList = workbookReader.getNumberSetList(); // numberSetList aus workbook lesen
+        WorkbookReader workbookReader = new WorkbookReader(workbook, 0);  //workbookReader starten
+        NumberSetList numberSetList = workbookReader.getNumberSetList();     // numberSetList aus workbook lesen
         System.out.println("Main: Job finished.");
+
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
         for (int id = 1; id < 6; id++) {
             executor.submit(new Worker(id, 1, numberSetList));
         }
-
         executor.shutdown();
-
-
-
-//        CustomThread customThread1 = new CustomThread(1, numberSetList);
-//        CustomThread customThread2 = new CustomThread(3, numberSetList);
-//        customThread1.run();
-//        customThread2.run();
 
 
     }
